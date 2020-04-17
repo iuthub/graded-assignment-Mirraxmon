5353c781caaf2e98c26fa20dbdb19cddc027548f
Mirraxmon <60439944+Mirraxmon@users.noreply.github.com>
2020-04-17 23:17:09 +0500

    finishrd all task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +13,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => 'auth'], function () {
+    // Route::resource('tasks', 'TaskController');
+    Route::get('/tasks', 'TaskController@index')->name('tasks.index');
+    Route::get('/tasks/create', 'TaskController@create')->name('tasks.create');
+    Route::post('/tasks', 'TaskController@store')->name('tasks.store');
+    Route::get('/tasks/{task}/edit', 'TaskController@edit')->name('tasks.edit');
+    Route::get('/tasks/{task}', 'TaskController@show')->name('tasks.show');
+    Route::patch('/tasks/{task}', 'TaskController@update')->name('tasks.update');
+    Route::delete('/tasks/{task}', 'TaskController@destroy')->name('tasks.destroy');
+    Route::get('/home', 'HomeController@index')->name('home');
 });
